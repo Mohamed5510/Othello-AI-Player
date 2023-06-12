c17f0cb1c5a7a1e890fcfa1df28a59519c164e74
mohamedkhaled127 <72905885+mohamedkhaled127@users.noreply.github.com>
2023-06-12 20:03:46 +0300

    Update TwoPlayersWindow.Designer.cs

--- Othello AI Player/TwoPlayersWindow.Designer.cs
+++ Othello AI Player/TwoPlayersWindow.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // TwoPlayersWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(428, 429);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "TwoPlayersWindow";
             this.Text = "TwoPlayersWindow";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
