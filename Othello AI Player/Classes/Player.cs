c350fffc1656afccab75d4458fa6c4ddbed3aee8
Mohamedsayed Ibrahim <85295104+Mohameddol99@users.noreply.github.com>
2023-06-12 19:03:52 +0300

    player class

--- Othello AI Player/Classes/Player.cs
+++ Othello AI Player/Classes/Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace Othello_AI_Player.Classes
 {
-    internal class Player
+    /*
+     * Class Name: Player.
+     * Description: class to be instantiated for each player in the game.
+     * Attributes: name, score, sign.
+     */
+    public class Player
     {
+        // Attributes
+        private string name;                    // Player name
+        private int score;                      // Player score
+        private Position_Color sign;                    // Coin color
+
+        // Costructor
+        public Player (string name, Position_Color sign)
+        {
+            this.name = name;
+            this.score = 0;
+            this.sign = sign;
+        }
+
+        // Setters & getters
+        public Position_Color Sign
+        {
+            get { return sign; }
+            set { sign = value; }
+        }
+
+        public string PlayerName
+        {
+            get { return name; }
+        }
+
+        public int PlayerScore
+        {
+            get { return score; }
+            set { score = value; }
+        }
+
+        // Method to get the opponent player coin color
+        public Position_Color GetOpponentSign()
+        {
+            Position_Color opponentSign;
+            if (this.sign == Position_Color.BLACK)
+            {
+                opponentSign = Position_Color.WHITE;
+            }
+            else
+            {
+                opponentSign = Position_Color.BLACK;
+            }
+
+            return opponentSign;
+        }
     }
 }
