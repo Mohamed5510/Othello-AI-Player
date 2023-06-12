a7dc42bede988574823c5fe9f7738464fd2eb41e
mahmoud abdellatif basiony <66041529+ModyBallba@users.noreply.github.com>
2023-06-12 20:16:48 +0300

    Update ZeroPlayersWindow.Designer.cs

--- Othello AI Player/ZeroPlayersWindow.Designer.cs
+++ Othello AI Player/ZeroPlayersWindow.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Othello_AI_Player
+namespace Othello_AI_Player
 {
     partial class ZeroPlayersWindow
     {
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ZeroPlayersWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(428, 429);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "ZeroPlayersWindow";
             this.Text = "ZeroPlayersWindow";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
